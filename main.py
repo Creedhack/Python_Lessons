0e84805ef93ae1bf2f4e2ba656e7a8f1311e105d
Creedhack <_@gmail.com>
2026-07-30 21:05:04 +0500

    Add main.py file

---
+++ main.py
@@ -0,0 +1,9 @@
+name = "Студент"
+print(f"Сәлем, {name}! IT және Python әлеміне қош келдің!")
+print("Түркістан А.Ясауи колледжінің болашақ үздік бағдарламашысы!")
+git init
+git add .
+git commit -m "My first python file"
+git branch -M main
+git remote add origin https://github.com/Creedhack/Python_Lessons.git
+git push -u origin main
