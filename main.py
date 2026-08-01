9e2046d3db994c4b0e26b76e6b663d300b0c70e5
Creedhack <_@gmail.com>
2026-08-02 03:31:42 +0500

    Lesson 4: Infinite guess number game with while loop and random

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-secret_number = 7
-print("=== САНДЫ ТАП ОЙЫНЫ ===!") 
-guess = int(input("1-ден 10-ға дейінгі санды енгізіңіз: ")) 
-if guess == secret_number:
-    print("Құттықтаймыз! Сіз дұрыс санды таптыңыз!") 
-elif guess < secret_number: 
-    print("Тым кішкентай! құпия сан бұдан үлкенірек.") 
-else:
-    print("Тым үлкен! құпия сан бұдан кіші.")  
+import random 
+secret_number = random.randint(1, 10) 
+guess = 0  
+print("=== ШЕКСІЗ САНДЫ ТАП ОЙЫНЫ ===")
+while guess != secret_number:
+    guess = int(input("1 мен 10 аралығындағы санды болжаңыз: "))
+    if guess < secret_number:
+        print("Кіші сан. Тағы бір рет көріңіз.")
+    elif guess > secret_number:
+        print("Үлкен сан. Тағы бір рет көріңіз.")
+    else:
+        print("УРА-А-А! Сіз құпия санды таптыңыз!")
