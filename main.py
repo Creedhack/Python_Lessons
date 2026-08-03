47f2f85b4a20671ab2aa4fb8f98c3415e2030cbb
Creedhack <_@gmail.com>
2026-08-04 01:23:34 +0500

    Lesson 7: The Weeknd playlist using Python Lists and for loop with append

--- main.py
+++ main.py
@@ -1,25 +1,14 @@
-hero_name = "Eminem" 
-hero_hp = 100 
-hero_damage = 25
-raper_name = "Slim Shady"
-raper_hp = 150
-raper_damage = 20
-print(f"=== ОЙЫН БАСТАЛДЫ ! ===\n")
-print(f"Eminem: {hero_name} (HP: {hero_hp}, Damage: {hero_damage})")
-print(f"Slim Shady: {raper_name} (HP: {raper_hp}, Damage: {raper_damage})\n")
-round = 1 
-while hero_hp > 0 and raper_hp > 0:
-    print(f"--- Раунд {round} ---")
-    raper_hp = raper_hp - hero_damage
-    print(f"{hero_name} ұрды ! {hero_damage} залал келтірді.")
-    print(f"{raper_name} Денсаулығы: {raper_hp}")
-    if raper_hp <= 0:
-        print(f"\n{raper_name} жеңілді ! {hero_name} жеңіске жетті !")
-        break
-    hero_hp = hero_hp - raper_damage
-    print(f"{raper_name} ұрды ! {raper_damage} залал келтірді.") 
-    print(f"{hero_name} Денсаулығы: {hero_hp}\n")
-    if hero_hp <= 0:
-        print(f"\n{hero_name} жеңілді ! {raper_name} жеңіске жетті !")
-    round = round + 1
-print(f"\n=== RAP BATTLE АЯҚТАЛДЫ ! ===") 
+inventory = ["Blinding lights", "Starboy", "Save Your Tears", "In Your Eyes", "Heartless", "Pray For Me"]
+print("=== THE WEEKND PLAYLIST ===")
+song_number = 1 
+for song in inventory:
+    print(f"- {song_number}. {song}")
+    song_number += 1
+print ("\n--------------------------------")
+print("Плэйлистке '(Cry For Me)' музыкасын қостыңыз !")
+inventory.append("Cry For Me")
+print("\n=== ӨҢДЕЛГЕН ПЛЭЙЛИСТ ===")
+song_number = 1
+for song in inventory:
+    print(f"- {song_number}. {song}")
+    song_number = song_number + 1
